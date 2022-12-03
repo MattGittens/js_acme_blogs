9b9c6e87a0c418d17846e28967f401be13b5b264
Matthew Gittens <msgittens@mail.fhsu.edu>
2022-12-03 16:35:19 -0500

    modification to function 1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 //function 1
+//creates element with text content and an optional class name
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function createElemWithText (elem = "p", textCont ="", className){
 const htmlElem = document.createElement(elem);
-htmlElem.textContext = textCont;
-if (!className) return htmlElem;
-htmlElem.classList.add(className);
+htmlElem.textContent = textCont;
+if(className) htmlElem.classList.add(className);
 return htmlElem;
-}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 
 //function 2
 function createSelectOptions(JSONData){
