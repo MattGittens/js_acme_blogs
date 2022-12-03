74f71dfd4ad75dd4eecbe602948dbcc7ef651daa
Matthew Gittens <msgittens@mail.fhsu.edu>
2022-12-03 17:46:18 -0500

    Function two

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 //function 1
 //creates element with text content and an optional class name
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function createElemWithText (elem = "p", textCont ="", className){
+function createElemWithText (elem = "p", textCont ='', className){
 const htmlElem = document.createElement(elem);
 htmlElem.textContent = textCont;
 if(className) htmlElem.classList.add(className);
 return htmlElem;
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//completed
 
 
 
 //function 2
+//creates select options for JSON data
 function createSelectOptions(JSONData){
-if(!JSONData) return;
+if(JSONData){
+const obj = JSON.parse(JSONData);
+obj.forEach(element => document.createElement(Option(element.user.name,element.user.id)));
+}
+ return;
 
 }
 
