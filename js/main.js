7f8d62d8f03d072122705fbf94b59c817f0e06b8
Matthew Gittens <msgittens@mail.fhsu.edu>
2022-12-03 21:09:10 -0500

    could not complet functions 2 and 3 moved passed it to 4 and completed it

--- js/main.js
+++ js/main.js
@@ -20,30 +20,52 @@ function createSelectOptions(jsonData){
 }
 
 //function 3
+
 function toggleCommentSection(postId) {
+    // If Post Id Is Passed, Return Undefined
     if (!postId) {
         return undefined;
-        //returns undefined if no post id
-
-
     } else {
-        const comments = document.querySelectorAll('[data-post-id]');
-        //selects all posts
-
-
-        for (let i = 0; i < comments.length; i++) {
-            const commentSection = comments[i];
+        // Else, Get All Comment Sections
+        const commentSections = document.querySelectorAll('[data-post-id]');
+        // Loop Through Each Comment Section
+        for (let i = 0; i < commentSections.length; i++) {
+            const commentSection = commentSections[i];
             // If Post Id Attribut Of Comment Section Is Equal To Post Id Passed Arg
             if (commentSection.getAttribute('data-post-id') === postId) {
-            commentSection.classList.toggle('hide');
-            return commentSection;
+                // Toggle Hide Class
+                commentSection.classList.toggle('hide');
+                // Return Comment Section Element
+                return commentSection;
             }
-            else
-            return null;
-           
         }
 
-        
-        
+        // If We Are Here, No Matching Post Id Is Found
+        // Return NULL
+        return null;
     }   
 }
+        
+//function 4
+function toggleCommentButton (postID) {
+
+    // if postID is not received, return
+    if (!postID) {
+      return;
+    }
+  
+    // select button having its value of "data-post-id" attribute = value of "postId"
+    const btnSelectedEl = document.querySelector(`button[data-post-id = "${postID}"`);
+  
+    if (btnSelectedEl != null) {
+      // if the textContent of button is 'Show Comments', change it to "Hide Comments", otherwise change to "Show Comments" by making use of ternary operator
+      btnSelectedEl.textContent === "Show Comments" ? (btnSelectedEl.textContent = "Hide Comments") : (btnSelectedEl.textContent = "Show Comments");
+    }
+  
+    // returning the selected button element
+    return btnSelectedEl;
+  };
+  
+  
+  // check for function
+  console.log(toggleCommentButton("btnToTest"));
